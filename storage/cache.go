63d6bab8a66fc1e5676defe260756bde8da3a31c
xmchz <21513006@zju.edu.cn>
2021-04-18 14:36:11 +0800

    update

--- storage/cache.go
+++ storage/cache.go
@@ -9,14 +9,14 @@ import (
 )
 
 const (
-	ctxKeySetCache    = "storage-cache-set-key"    // set cache
+	ctxKeyCache       = "storage-cache-set-key"    // set cache
 	ctxKeyDeleteCache = "storage-cache-delete-key" // delete cache
 )
 
 var ErrCacheKeyNotExist = errors.New("cache key not exist in context")
 
 func CtxSetKey(ctx context.Context, key string) context.Context {
-	return context.WithValue(ctx, ctxKeySetCache, key)
+	return context.WithValue(ctx, ctxKeyCache, key)
 }
 
 func CtxDeleteKey(ctx context.Context, key string) context.Context {
@@ -29,7 +29,7 @@ type Cache struct {
 }
 
 func (s *Cache) Find(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
-	key, err := s.key(ctx, ctxKeySetCache)
+	key, err := s.key(ctx, ctxKeyCache)
 	if errors.Is(err, ErrCacheKeyNotExist) {
 		return s.Storage.Find(ctx, dest, query, args...)
 	}
@@ -40,7 +40,7 @@ func (s *Cache) Find(ctx context.Context, dest interface{}, query string, args .
 }
 
 func (s *Cache) FindList(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
-	key, err := s.key(ctx, ctxKeySetCache)
+	key, err := s.key(ctx, ctxKeyCache)
 	if errors.Is(err, ErrCacheKeyNotExist) {
 		return s.Storage.FindList(ctx, dest, query, args...)
 	}
@@ -50,7 +50,7 @@ func (s *Cache) FindList(ctx context.Context, dest interface{}, query string, ar
 	})
 }
 
-func (s *Cache) Create(ctx context.Context, query string, args ...interface{}) (sql.Result, error){
+func (s *Cache) Create(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
 	res, err := s.Storage.Create(ctx, query, args...)
 	if err != nil {
 		return res, err
@@ -60,36 +60,31 @@ func (s *Cache) Create(ctx context.Context, query string, args ...interface{}) (
 }
 
 func (s *Cache) Update(ctx context.Context, query string, args ...interface{}) error {
-	key, err := s.key(ctx, ctxKeySetCache)
-	if errors.Is(err, ErrCacheKeyNotExist) {
-		return s.Storage.Update(ctx, query, args...)
-	}
 	if err := s.Storage.Update(ctx, query, args...); err != nil {
 		return err
 	}
-	s.deleteCache(key)
+	if key, err := s.key(ctx, ctxKeyCache); err == nil {
+		s.deleteCache(key)
+	}
 	s.deleteCtxCache(ctx)
 	return nil
 }
 
 func (s *Cache) Delete(ctx context.Context, query string, args ...interface{}) error {
-	key, err := s.key(ctx, ctxKeySetCache)
-	if errors.Is(err, ErrCacheKeyNotExist) {
-		return s.Storage.Delete(ctx, query, args...)
-	}
 	if err := s.Storage.Delete(ctx, query, args...); err != nil {
 		return err
 	}
-	s.deleteCache(key)
+	if key, err := s.key(ctx, ctxKeyCache); err == nil {
+		s.deleteCache(key)
+	}
 	s.deleteCtxCache(ctx)
 	return nil
 }
 
-
 func (s *Cache) key(ctx context.Context, ctxKey string) (string, error) {
 	key, ok := ctx.Value(ctxKey).(string)
 	if !ok {
-		log.Debug("%s not exist in ctx", ctxKey)
+		// log.Debug("%s not exist in ctx", ctxKey)
 		return "", ErrCacheKeyNotExist
 	}
 	return key, nil
